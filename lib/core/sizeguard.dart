5fe23e8a4e54f3ef504a7ffff2b98da992ec8c44
vaxpos <vaxpsy@gmail.com>
2025-10-22 05:30:23 +0300

    90

--- lib/core/sizeguard.dart
+++ lib/core/sizeguard.dart
@@ -2,18 +2,16 @@ import 'dart:ui';
 
 import 'package:window_manager/window_manager.dart';
 
-/// 🔒 هذا الكلاس يراقب حجم النافذة ويمنع تصغيرها تحت الحد الأدنى مع دعم الاستجابة
 class SizeGuard extends WindowListener {
   @override
   void onWindowResize() async {
-    const minWidth = 768.0; // حد أدنى للجوال/تابلت
+    const minWidth = 768.0;
     const minHeight = 600.0;
-    const maxWidth = 1920.0; // حد أقصى للشاشات الكبيرة
+    const maxWidth = 1920.0; 
     const maxHeight = 1080.0;
 
     final size = await windowManager.getSize();
 
-    // تصحيح الحجم إذا كان أصغر من الحد الأدنى
     if (size.width < minWidth || size.height < minHeight) {
       await windowManager.setSize(
         Size(
@@ -23,7 +21,6 @@ class SizeGuard extends WindowListener {
       );
     }
 
-    // تصحيح الحجم إذا كان أكبر من الحد الأقصى
     if (size.width > maxWidth || size.height > maxHeight) {
       await windowManager.setSize(
         Size(
